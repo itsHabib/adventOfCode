f1c94c780ac67e2f33aabda3b1d70fbf58f92daa
Michael Habib <michael.habib@couchbase.com>
2024-12-02 13:00:31 -0600

    dont need heap

--- 2024/day1/main.go
+++ 2024/day1/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"bufio"
-	"container/heap"
 	"fmt"
 	"log"
 	"math"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -21,12 +21,10 @@ func main() {
 
 	var (
 		s      = bufio.NewScanner(f)
-		left   = new(queue)
-		right  = new(queue)
 		rcount = make(map[int]int)
+		left   []int
+		right  []int
 	)
-	heap.Init(left)
-	heap.Init(right)
 	for s.Scan() {
 		line := s.Text()
 		if line == "" {
@@ -44,54 +42,31 @@ func main() {
 		}
 		rcount[r]++
 
-		heap.Push(left, l)
-		heap.Push(right, r)
+		left = append(left, l)
+		right = append(right, r)
 	}
 
 	if err := s.Err(); err != nil {
 		log.Fatal(err)
 	}
 
+	sort.Ints(left)
+	sort.Ints(right)
+
 	var (
 		part1 int
 		part2 int
+		i     int
 	)
-	for left.Len() > 0 && right.Len() > 0 {
-		l := heap.Pop(left).(int)
-		r := heap.Pop(right).(int)
+	for i < len(left) && i < len(right) {
+		l := left[i]
+		r := right[i]
 
 		part1 += int(math.Abs(float64(r - l)))
 		part2 += l * rcount[l]
+		i++
 	}
 
 	fmt.Println("part 1 answer:", part1)
 	fmt.Println("part 2 answer:", part2)
-}
-
-var _ heap.Interface = (*queue)(nil)
-
-type queue struct {
-	items []int
-}
-
-func (q queue) Len() int {
-	return len(q.items)
-}
-
-func (q queue) Less(i, j int) bool {
-	return q.items[i] < q.items[j]
-}
-
-func (q *queue) Swap(i, j int) {
-	q.items[i], q.items[j] = q.items[j], q.items[i]
-}
-
-func (q *queue) Push(x any) {
-	q.items = append(q.items, x.(int))
-}
-
-func (q *queue) Pop() any {
-	x := q.items[len(q.items)-1]
-	q.items = q.items[:len(q.items)-1]
-	return x
 }
